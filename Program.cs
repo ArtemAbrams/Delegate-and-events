8c1fcea6d4f1923def5d3c2ed7e5e917ffe70db1
artem <barishartem@gmail.com>
2022-07-31 12:39:42 +0300

    Modified file program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿
 Print print;
 print = Hello;
+print += SomeMethod;
+print();
+print -= Hello;
 print();
 void Hello() => Console.WriteLine("Hi");
+void SomeMethod() => Console.WriteLine("How are you");
 delegate void Print();
