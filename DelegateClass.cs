94ed841e10237022a09468be80cf3ee529b3c232
artem <barishartem@gmail.com>
2022-07-31 16:27:48 +0300

    New class Delegate_Class

---
+++ DelegateClass.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp
+{
+    delegate void PrintMessage(string message, int sum);
+
+    class DelegateClass
+    {
+        int sum;
+        PrintMessage? prtmsg = null;
+
+        public DelegateClass(int sum)
+        {
+            this.sum = sum;
+        }
+
+        public void AddMethod(PrintMessage prtmsg)
+        {
+            this.prtmsg += prtmsg;
+        }
+
+        public void DeleteMethod(PrintMessage prtmsg)
+        {
+            if (this.prtmsg == null)
+                Console.WriteLine("Cringe");
+            else
+                this.prtmsg -= prtmsg;
+        }
+        public void Add(int sum)
+        {
+            if (prtmsg != null)
+            {
+                this.sum += sum;
+                prtmsg.Invoke("You add money, your balance", this.sum);
+            }
+            else
+                Console.WriteLine("No method");
+        }
+
+        public void Taken(int sum)
+        {
+            if (prtmsg == null)
+            {
+                Console.WriteLine("No method");
+            }
+            else
+            {
+                if (this.sum < sum)
+                {
+                    prtmsg.Invoke("You do not have enough money, your balance", this.sum);
+                }
+                else
+                {
+                    prtmsg.Invoke("You take money your balance", this.sum);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
-﻿
-/*Print print;
-print = Hello;
-print += SomeMethod;
-print();
-print -= Hello;
-print();
-void Hello() => Console.WriteLine("Hi");
-void SomeMethod() => Console.WriteLine("How are you");*/
-
+﻿using  ConsoleApp;
 using System.ComponentModel.Design;
-
 Add? add = SomeMethod_2;
 add+=Somemethod_3;
 int? result = add(4, 5);
@@ -17,12 +7,18 @@ Console.WriteLine(result);
 int? result_1 = add.Invoke(5, 6);
 Console.WriteLine(result_1);
 add -= SomeMethod_2;
-add -= Somemethod_3;
-if(add != null)
+if (add != null)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
     result = add.Invoke(10,10);
+    Console.WriteLine(result);
+    Console.ResetColor();
+}
 else
     Console.WriteLine("No method");
 int SomeMethod_2(int x, int y) => x + y;
 int Somemethod_3(int x, int y) => x * y;
+
 delegate void Print();
+
 delegate int Add(int x, int y);
