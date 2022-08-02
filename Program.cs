a86d3269ed4f5e90e65ce3871b4c7a60084df262
artem <barishartem@gmail.com>
2022-08-02 14:20:30 +0300

    Anonymous methods

--- Program.cs
+++ Program.cs
@@ -1,44 +1,18 @@
-﻿using  ConsoleApp;
-using System.ComponentModel.Design;
-/*Add? add = SomeMethod_2;
-add+=Somemethod_3;
-int? result = add(4, 5);
-Console.WriteLine(result);
-int? result_1 = add.Invoke(5, 6);
-Console.WriteLine(result_1);
-add -= SomeMethod_2;
-if (add != null)
+﻿using ConsoleApp;
+
+Sum sum = delegate(int x, int y)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    result = add.Invoke(10,10);
-    Console.WriteLine(result);
-    Console.ResetColor();
-}
-else
-    Console.WriteLine("No method");
-int SomeMethod_2(int x, int y) => x + y;
-int Somemethod_3(int x, int y) => x * y;
-
-delegate void Print();
-
-delegate int Add(int x, int y);*/
-DelegateClass dlgcls = new DelegateClass(200);
-
-void PrintGreenColour(string message, int sum)
+    return x + y;
+};
+Print("Your debt", 45, delegate(string message, int debt)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(message + " " + sum);
-    Console.ResetColor();
+  Console.WriteLine(message + " " + debt );
+});
+
+void Print(string message, int debt, Printt print)
+{
+    print(message, debt);
 }
 
-void PrintRedColour(string message, int sum)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(message + " "+sum);
-    Console.ResetColor();
-}
-dlgcls.AddMethod(PrintGreenColour);
-dlgcls.Add(500);
-dlgcls.DeleteMethod(PrintGreenColour);
-dlgcls.AddMethod(PrintRedColour);
-dlgcls.Taken(600);
+delegate void Printt(string message, int debt);
+delegate int Sum(int x, int y);
