c9cbf75951a670d700c5c17872f3dd68de7db6ce
artem <barishartem@gmail.com>
2022-08-02 17:02:57 +0300

    Modified class Lumbda

---
+++ Lumbda.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp;
+class Lumbda
+{
+  public int[] arr
+  {
+    get
+    {
+      return arr;
+    }
+    set
+    {
+      arr = value;
+    }
+  }
+  public Checked check;
+
+  public Lumbda(int[] arr, Checked check)
+  {
+    this.arr = arr;
+    this.check = check;
+  }
+  public int Sum()
+  {
+    var sum = 0;
+    foreach (var i in arr)
+    {
+      if (check(i))
+      {
+        sum += i;
+      }
+    }
+
+    return sum;
+  }
+  public delegate bool Checked(int i);
+}
