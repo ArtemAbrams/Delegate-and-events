812a280d590295b327c0537ce307f15690b8e59e
artem <barishartem@gmail.com>
2022-08-07 19:42:51 +0300

    converting class Euro to Dollar and vice versa

---
+++ Dollar.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp;
+
+class Dollar
+{
+    public decimal Sum { get; set; }
+
+    public static implicit operator Euro(Dollar dollar)
+    {
+        return new Euro() {  Sum = dollar.Sum / 1.12M };
+    }
+    public static implicit operator Dollar(Euro euro)
+    {
+        return new Euro() { Sum = euro.Sum * 1.12M };
+    }
+}
+class Euro
+{
+    public decimal Sum { get; set; }
+    
+}
