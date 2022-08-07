a82000b0d7edba29b2ed3d3fdd5537b7ce5de5d0
artem <barishartem@gmail.com>
2022-08-07 13:18:11 +0300

    add operator +

--- Program.cs
+++ Program.cs
@@ -1,45 +1,5 @@
 ﻿using ConsoleApp;
-
-/*Sum sum = delegate(int x, int y)
-{
-    return x + y;
-};
-Print("Your debt", 45, delegate(string message, int debt)
-{
-  Console.WriteLine(message + " " + debt );
-});
-
-void Print(string message, int debt, Printt print)
-{
-    print(message, debt);
-}
-
-delegate void Printt(string message, int debt);
-delegate int Sum(int x, int y);*/
-int[] arr = new int[10];
-    Lumbda lumbda = new Lumbda(arr ,(int x) =>
-    {
-        return x > 500;
-    });
-lumbda.PrintArray();
-int result = lumbda.Sum();
-Console.WriteLine(result);
-Events events = new Events(200);
-void PrintRed(string message)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(message);
-    Console.ResetColor();
-}
-
-void PrintGreen(string message)
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(message);
-    Console.ResetColor();
-}
-events.AddEvent(PrintGreen);
-events.Add(1000);
-events.RemoveEvent(PrintGreen);
-events.AddEvent(PrintRed);
-events.Take(300);
+Bread bread = new Bread(45, 90);
+Butter butter = new Butter(60, 89);
+Sandwich sandwich = bread + butter;
+Console.WriteLine(sandwich.Quality+" " + sandwich.Weight);
---
+++ Sandwich.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp
+{
+
+ class Butter
+ {
+  public int Weight { get; set; }
+  public int Quality { get; set; }
+
+  public Butter(int weight, int quality)
+  {
+   this.Quality = quality;
+   this.Weight = weight;
+  }
+ }
+
+ class Bread
+ {
+  public int Weight { get; set; }
+  public int Quality { get; set; }
+
+  public Bread(int weight, int quality)
+  {
+   this.Weight = weight;
+   this.Quality = quality;
+  }
+
+  public static Sandwich operator +(Bread bread, Butter butter)
+  {
+   return new Sandwich((bread.Quality+butter.Quality)/2, butter.Weight+bread.Weight);
+  }
+ }
+
+ class Sandwich
+ {
+  public int Quality;
+  public int Weight;
+
+  public Sandwich(int quality, int weight)
+  {
+   this.Quality = quality;
+   this.Weight = weight;
+  }
+ }
+}
